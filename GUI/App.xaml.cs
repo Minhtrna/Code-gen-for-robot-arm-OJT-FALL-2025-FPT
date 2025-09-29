96e4bcef56af564d9f4b19fae0b6e00038b21f1c
Minh <quangtran102005@gmail.com>
2025-09-29 19:59:52 +0700

    Update App.xaml.cs

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -4,10 +4,6 @@ namespace GUI
 {
     public partial class App : Application
     {
-        // Có thể override OnStartup nếu cần setup đặc biệt
-        // protected override void OnStartup(StartupEventArgs e)
-        // {
-        //     base.OnStartup(e);
-        // }
+        
     }
 }
