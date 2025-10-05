7ff9a7499876839a09fb2f3e52caa70c4d3799f9
Minh <quangtran102005@gmail.com>
2025-10-05 13:59:05 +0700

    Update 1.5.1

---
+++ GUI/Views/CameraView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GUI.Views
+{
+    public partial class CameraView : UserControl
+    {
+        public CameraView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GUI/Views/ValidateSimulateView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GUI.Views
+{
+    public partial class ValidateSimulateView : UserControl
+    {
+        public ValidateSimulateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
